51742c95b131fefcad098dc384ddf4b59e6db11f
pandrzejewski1 <piotrandrzejewski@hotmail.com>
2020-06-19 22:32:03 +0200

    Initial Commit

---
+++ Niania/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Niania
+//
+//  Created by Ja on 19/06/2020.
+//  Copyright © 2020 Ja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
