670043b380c42c4e780fd4b61874db10eafcf411
pandrzejewski1 <piotrandrzejewski@hotmail.com>
2020-06-19 23:29:10 +0200

    Check for mic permission.

--- Niania/ViewController.swift
+++ Niania/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        requestMicPermission {[weak self] granted in
+            if (granted) {
+                
+            }
+            else {
+                let alert = UIAlertController(title: "No mic permission", message: "Please enable Mic permiision in app settings", preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(UIAlertAction.init(title: "OK", style: .default, handler: nil))
+                DispatchQueue.main.async {
+                    self?.present(alert, animated: true, completion: nil)
+                }
+            }
+        }
     }
 
-
+    private func requestMicPermission(onResult: ((Bool) -> Void)?) {
+        let permission = AVAudioSession.sharedInstance().recordPermission
+    
+        switch permission {
+        case .denied:
+            onResult?(false)
+        case .granted:
+            onResult?(true)
+        case .undetermined:
+            AVAudioSession.sharedInstance().requestRecordPermission { granted in
+                onResult?(granted)
+            }
+        }
+    }
 }
 
